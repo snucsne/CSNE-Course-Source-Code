ed47e61b5d4fbff12120b971377daa9615f344da
beskridge <beskridge@snu.edu>
2015-10-12 13:25:04 -0500

    Updated loop examples for CSNE3673 Bash Programming

--- CSNE3673-Net-Admin-I/examples/bash-programming/loop-examples.sh
+++ CSNE3673-Net-Admin-I/examples/bash-programming/loop-examples.sh
@@ -58,3 +58,11 @@ done
 # ******************************************************************************
 # While loop examples
 
+echo Executing a while loop to countdown to blastoff
+counter=5
+while [ "$counter" -gt 0 ]; do
+    echo Countdown [$counter]
+    counter=$(($counter - 1))
+done
+echo Blastoff
+
