a107482b6ac7fe213e362df97f5d82a123694390
beskridge <beskridge@snu.edu>
2015-10-13 19:00:17 -0500

    Added awk and sed examples for CSNE3673

---
+++ CSNE3673-Net-Admin-I/examples/awk/awk-examples.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+# 
+# Copyright (c) 2015 Southern Nazarene University Computer Science/Network Engineering Department
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# ******************************************************************************
+# (g)awk examples
+
+# Selects all the lines in the cars file that contain the string 'chevy'
+echo "Selecting all lines that contain the string 'chevy'"
+gawk '/chevy/' cars
+echo
+
+
+# Selects all the lines in the cars file that contain the string 'chevy' and
+# print the year & make:
+echo "Selecting all lines that contain 'chevy' and print the year and make"
+gawk '/chevy/ {print $3, $1}' cars
+echo
+
+
+# Selects all the lines that contain the letter h in the first field (make):
+echo "Selecting all lines that contain the letter h in the first field"
+gawk '$1 ~ /h/' cars
+echo
+
+
+# Selects all the lines that contain the letter h as the first character in
+# the first field (make):
+echo "Selecting all lines that contain h as the first character in the first field"
+gawk '$1 ~ /^h/' cars
+echo
+
+
+# Selects all the lines in the cars file in which the third column (year) ends in
+# a 5 and prints the year, make, & price:
+echo "Selecting lines in which the third comlumn ends in 5 and prints the year, make and price"
+gawk '$3 ~ /5$/ {print $3, $1, "$" $5}' cars
+echo
+
+
+# Selects all the lines where the price is greater than or equal to $2000 and less than $9000:
+echo "Selecting lines where the price is greater than or equal to $2000 and less than $9000"
+gawk '2000 <= $5 && $5 < 9000' cars
+echo
---
+++ CSNE3673-Net-Admin-I/examples/sed/sed-examples.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+# 
+# Copyright (c) 2015 Southern Nazarene University Computer Science/Network Engineering Department
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# ******************************************************************************
+# sed examples
+
+# Select the lines from new that contain the string line:
+echo "============================================================"
+echo "Select the lines from new that contain the string line"
+sed -n '/line/ p' new
+echo
+
+
+# Selects and displays lines 3 through 6 of the file new:
+echo "============================================================"
+echo "Selects and displays lines 3 through 6 of the file new"
+sed -n '3,6 p' new
+echo
+
+
+# Uses a program-file to accomplish the same thing:
+echo "============================================================"
+echo "Uses a program-file to accomplish the same thing"
+echo "  print3_6 contents:"
+cat print3_6
+echo "  sed output:"
+sed -n -f print3_6 new
+echo
+
+
+# Quits after displaying the first 5 lines of the file new:
+echo "============================================================"
+echo "Quits after displaying the first 5 lines of the file new"
+sed '5 q' new
+echo
+
+
+# Selects line 2 and appends a NEWLINE and the text AFTER:
+echo "============================================================"
+echo "Selects line 2 and appends a NEWLINE and the text AFTER"
+sed '2 a\
+AFTER.' new
+echo
+
+
+# Selects lines that begin with a non-empty character and replaces it
+# with a TAB and the matched character (effectively inserting a TAB):
+echo "============================================================"
+echo "Insert a TAB in lines that begin with a non-empty character"
+sed 's/^./\t&/' new
+echo
+
+
+# Substitutes the word sentence for the word line in the file new
+# and displays each line where a substitution occurs:
+echo "============================================================"
+echo "Substituting 'sentence' for 'line'"
+sed 's/line/sentence/p' new
+echo
